3513acf82073de11a444bc4fd22854519a0ab05b
Felix <felixtwoli21@gmail.com>
2023-03-28 20:53:00 +0300

    Add textinput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-import { Container, Box } from '@chakra-ui/react'
+import { Container, Box, Textarea } from '@chakra-ui/react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import TextInput from './components/TextInput'
 
 const App = () => {
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='3xl' centerContent>
         <Header />
+        <TextInput extractKeyphrases/>
         <Footer />
       </Container>
 
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import { useState } from 'react'
-import { Textarea, Button } from '@chakra-ui/react'
+import { Textarea, Button, useToast } from '@chakra-ui/react'
 
-const TextInput = () => {
+const TextInput = ({extractKeyphrases}) => {
+  const [text, setText] = useState('');
+  const toast = useToast();
+
+  const submitText = () => {
+    if (text === '') {
+      toast({
+        title: "Text is empty",
+        description: "Please enter some text to extract keyphrases",
+        status: "error",
+        duration: 5000,
+        isClosable: false,
+      });
+    }else {
+        extractKeyphrases(text);
+      
+    }
+  };
   return (
-    <div>TextInput</div>
+    <>
+      <Textarea bg='blue.400' color='white' padding={4} marginTop='6' height={200} value={text}
+        onChange={(e) => setText(e.target.value)} />
+
+      <Button
+      bg='blue.500'
+      color='white'
+      width='100%'
+      marginTop='4'
+      _hover={{ bg: 'blue.700' }}
+      onClick={(submitText)}
+      >
+        Extract Keyphrase
+      </Button>
+      
+    </>
   )
 }
 
